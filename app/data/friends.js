fddb6af4ca8e6f8d9b6d3dc3180c0a4defe414ae
Brandon B <broe5392@yahoo.com>
2018-08-16 21:00:52 -0400

    friendFinder skeleton

---
+++ app/data/friends.js
@@ -0,0 +1,52 @@
+var friendArray = [
+    {
+    "name":"Kenny M.",
+    "photo":"#",
+    "scores":[
+        2,
+        1,
+        2,
+        3,
+        1,
+        1,
+        2,
+        3,
+        4,
+        1
+      ]
+  },
+  {
+    "name":"Christy F.",
+    "photo":"#",
+    "scores":[
+        3,
+        1,
+        4,
+        4,
+        3,
+        2,
+        2,
+        3,
+        4,
+        1
+      ]
+  },
+  {
+    "name":"David R.",
+    "photo":"#",
+    "scores":[
+        5,
+        4,
+        4,
+        4,
+        5,
+        5,
+        4,
+        5,
+        4,
+        4
+      ]
+  }
+];
+
+module.exports = friendArray;
